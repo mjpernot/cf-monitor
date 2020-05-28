4fdd89abd8455eedf40df97b2da07feb1bedecc1
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-05-28 11:13:35 +0100

    Update test/unit/cf_monitor/run_program.py v0.2.0

--- test/unit/cf_monitor/run_program.py
+++ test/unit/cf_monitor/run_program.py
@@ -35,33 +35,6 @@ import version
 __version__ = version.__version__
 
 
-class ProgramLock(object):
-
-    """Class:  ProgramLock
-
-    Description:  Class stub holder for gen_class.ProgramLock class.
-
-    Methods:
-        __init__ -> Class initialization.
-
-    """
-
-    def __init__(self, cmdline, flavor):
-
-        """Method:  __init__
-
-        Description:  Class initialization.
-
-        Arguments:
-            (input) cmdline -> Argv command line.
-            (input) flavor -> Lock flavor ID.
-
-        """
-
-        self.cmdline = cmdline
-        self.flavor = flavor
-
-
 class UnitTest(unittest.TestCase):
 
     """Class:  UnitTest
@@ -110,30 +83,10 @@ class UnitTest(unittest.TestCase):
 
         self.cfg = CfgTest()
         self.args_array = {"-c": "config_file", "-d": "config"}
-        self.proglock = ProgramLock(["cmdline"], "FlavorID")
-
-    @mock.patch("cf_monitor.gen_libs.load_module")
-    @mock.patch("cf_monitor.gen_class.ProgramLock")
-    def test_programlock_false(self, mock_lock, mock_cfg):
-
-        """Function:  test_programlock_false
-
-        Description:  Test with ProgramLock returns False.
-
-        Arguments:
-
-        """
-
-        mock_lock.side_effect = cf_monitor.gen_class.SingleInstanceException
-        mock_cfg.return_value = self.cfg
-
-        with gen_libs.no_std_out():
-            self.assertFalse(cf_monitor.run_program(self.args_array))
 
     @mock.patch("cf_monitor.monitor", mock.Mock(return_value=True))
     @mock.patch("cf_monitor.gen_libs.load_module")
-    @mock.patch("cf_monitor.gen_class.ProgramLock")
-    def test_run_monitor(self, mock_lock, mock_cfg):
+    def test_run_monitor(self, mock_cfg):
 
         """Function:  test_run_monitor
 
@@ -143,7 +96,6 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        mock_lock.return_value = self.proglock
         mock_cfg.return_value = self.cfg
 
         self.assertFalse(cf_monitor.run_program(self.args_array))
