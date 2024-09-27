aed00fbb0f6f104d0be8262df6c61f547f681d3e
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2024-09-27 08:50:21 +0100

    Modified parsing of argument line v1.0.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,14 @@ All notable changes to this project will be documented in this file.
 The format is based on "Keep a Changelog".  This project adheres to Semantic Versioning.
 
 
+## [1.0.4] - 2024-09-27
+- Updated simplejson==3.13.2 for Python 3
+- Updated python-lib to v3.0.4
+
+### Changed
+- main: Removed parsing from gen_class.ArgParser call and called arg_parse2 as part of "if" statement.
+
+
 ## [1.0.3] - 2024-08-08
 - Updated simplejson==3.13.2
 - Added certifi==2019.11.28
--- test/unit/cf_monitor/main.py
+++ test/unit/cf_monitor/main.py
@@ -40,6 +40,7 @@ class ArgParser(object):
         arg_require
         get_args
         get_val
+        arg_parse2
 
     """
 
@@ -59,6 +60,7 @@ class ArgParser(object):
         self.opt_req2 = True
         self.dir_perms_chk = None
         self.dir_perms_chk2 = True
+        self.argparse2 = True
 
     def arg_dir_chk(self, dir_perms_chk):
 
@@ -112,6 +114,18 @@ class ArgParser(object):
 
         return self.args_array.get(skey, def_val)
 
+    def arg_parse2(self):
+
+        """Method:  arg_parse2
+
+        Description:  Method stub holder for gen_class.ArgParser.arg_parse2.
+
+        Arguments:
+
+        """
+
+        return self.argparse2
+
 
 class ProgramLock(object):
 
@@ -146,6 +160,8 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp
+        test_arg_parse2_false
+        test_arg_parse2_true
         test_help_true
         test_help_false
         test_arg_req_false
@@ -175,6 +191,40 @@ class UnitTest(unittest.TestCase):
         self.args2.args_array = {
             "-c": "config_file", "-d": "config_dir", "-y": "Flavor"}
         self.proglock = ProgramLock(["cmdline"], "FlavorID")
+
+    @mock.patch("cf_monitor.gen_class.ArgParser")
+    def test_arg_parse2_false(self, mock_arg):
+
+        """Function:  test_arg_parse2_false
+
+        Description:  Test arg_parse2 returns false.
+
+        Arguments:
+
+        """
+
+        self.args.argparse2 = False
+
+        mock_arg.return_value = self.args
+
+        self.assertFalse(cf_monitor.main())
+
+    @mock.patch("cf_monitor.gen_libs.help_func")
+    @mock.patch("cf_monitor.gen_class.ArgParser")
+    def test_arg_parse2_true(self, mock_arg, mock_help):
+
+        """Function:  test_arg_parse2_true
+
+        Description:  Test arg_parse2 returns true.
+
+        Arguments:
+
+        """
+
+        mock_arg.return_value = self.args
+        mock_help.return_value = True
+
+        self.assertFalse(cf_monitor.main())
 
     @mock.patch("cf_monitor.gen_libs.help_func")
     @mock.patch("cf_monitor.gen_class.ArgParser")
